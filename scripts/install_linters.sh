2c9ec797d1e0fa9519611453a989b26f892688ce
Luis Witz <luiswitz@gmail.com>
2022-05-18 09:42:22 -0300

    Add linter scripts

---
+++ scripts/install_linters.sh
@@ -0,0 +1,14 @@
+# rustywind
+npm install -g rustywind
+
+# tailwind language-server
+npm install -g @tailwindcss/language-server
+
+# prettierd
+npm install -g @fsouza/prettierd
+
+# erb_lint
+gem install erb_lint
+
+# stylua
+cargo install stylua
