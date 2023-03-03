ce5fd29523e9e02c4600342896f590435eb63aa0
Luis Witz <luiswitz@gmail.com>
2023-03-03 10:32:45 -0300

    Manage sources options for null-ls

--- nvim/.config/nvim/lua/null-ls-config/init.lua
+++ nvim/.config/nvim/lua/null-ls-config/init.lua
@@ -2,10 +2,11 @@ local null_ls = require("null-ls")
 
 local formatting = null_ls.builtins.formatting
 
+vim.lsp.buf.format(nil, 2000)
+
 local sources = {
 	formatting.eslint,
-	formatting.rustywind,
-	formatting.erb_lint,
+  formatting.erb_lint,
 	formatting.jq,
 	formatting.prettier,
 	formatting.rubocop,
@@ -13,5 +14,5 @@ local sources = {
 }
 
 null_ls.setup({
-	sources = soruces,
+	sources = sources,
 })
