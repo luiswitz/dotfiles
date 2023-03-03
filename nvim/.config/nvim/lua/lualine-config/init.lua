7932a100b336dd2a80d175dc15c3252237a97071
Luis Witz <luiswitz@gmail.com>
2023-03-03 10:48:58 -0300

    Cleanup lualine config

--- nvim/.config/nvim/lua/lualine-config/init.lua
+++ nvim/.config/nvim/lua/lualine-config/init.lua
@@ -13,34 +13,15 @@ local function diff_source()
   end
 end
 
--- local function current_date()
---   return string.sub(os.date "%x", 1, 5)
--- end
-
 local function current_working_dir()
   local cwd = string.sub(vim.fn.getcwd(), 12)
   return "~" .. cwd
 end
 
--- @TODOUA: keep tinkering with theme and section layouts!
--- @TODOUA: roll my own status bar or try expressline?
--- https://github.com/nvim-lualine/lualine.nvim
-local custom_auto = require "lualine.themes.auto"
-custom_auto.terminal.a.bg = "#1e90ff"
-custom_auto.normal.a.bg = "#131313"
-custom_auto.normal.a.fg = "#6d7275"
-custom_auto.normal.c.fg = "#E2E5DC"
-custom_auto.normal.c.bg = "#131313"
-custom_auto.command.a.bg = "#1e90ff"
-custom_auto.command.b.fg = "#1e90ff"
-custom_auto.replace.a.bg = "#C83434"
-custom_auto.visual.a.bg = "#725191"
-custom_auto.visual.b.fg = "#1e90ff"
-
 require("lualine").setup {
   options = {
     icons_enabled = true,
-    theme = 'tokyonight',
+    theme = 'catppuccin',
     component_separators = { left = "⦚", right = " ⦚" },
     section_separators = { left = " ", right = " " },
     disabled_filetypes = {},
@@ -50,7 +31,7 @@ require("lualine").setup {
   sections = {
     lualine_a = { "mode" },
     lualine_b = {
-      { "b:gitsigns_head", icon = { "", color = { fg = "#f2891c" } } },
+      { "b:gitsigns_head", icon = { "" } },
       { "diff", source = diff_source },
       { "diagnostics", sources = { "nvim_diagnostic" } },
     },
@@ -60,18 +41,17 @@ require("lualine").setup {
     },
     lualine_x = { { "filetype", icon_only = true } },
     lualine_y = {
-      { current_buffer_number, color = { fg = "#A9A9A9" } },
-      { current_working_dir, color = { fg = "#A9A9A9" } },
-      -- { current_date, color = { fg = "#A9A9A9" } },
+      { current_buffer_number },
+      { current_working_dir },
     },
-    lualine_z = { { "location", color = { fg = "#6d7275", bg = "#131313" } } },
+    lualine_z = { { "location" } },
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
     lualine_c = { { "filename", path = 1, symbols = { modified = "[]", readonly = " " } } },
     lualine_x = { "location" },
-    lualine_y = { { current_buffer_number, color = { fg = "#A9A9A9" } } },
+    lualine_y = { { current_buffer_number } },
     lualine_z = {},
   },
   extensions = {},
