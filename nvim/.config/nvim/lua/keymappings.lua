85cfeb2aa42883ce36b206ab0baeb24e6d33c225
Luis Witz <luiswitz@gmail.com>
2023-03-25 16:47:11 -0300

    Add some key mappings

--- nvim/.config/nvim/lua/keymappings.lua
+++ nvim/.config/nvim/lua/keymappings.lua
@@ -85,3 +85,18 @@ vim.api.nvim_set_keymap(
 
 -- open alternate file
 vim.api.nvim_set_keymap("n", "<Leader>av", ":lua require('custom').alt_file()<CR>", { noremap = true, silent = true })
+
+-- move selected lines
+vim.keymap.set('v', 'J', ":move +2<CR>gv=gv")
+vim.keymap.set('v', 'K', ":move -2<CR>gv=gv")
+
+-- keep cursor in the same place when joining lines
+vim.keymap.set('n', 'J', 'mzJ`z')
+
+-- preserve copied content
+vim.keymap.set('x', '<leader>p', "\"_dp")
+
+-- yank to system clipboard
+vim.keymap.set('n', '<leader>y', "\"+y")
+vim.keymap.set('v', '<leader>y', "\"+y")
+vim.keymap.set('n', '<leader>Y', "\"+Y")
