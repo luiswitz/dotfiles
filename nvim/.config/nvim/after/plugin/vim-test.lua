e8a002bb8d9f94994dc9a2b63f4894a9184c1b7a
Luis Witz <luiswitz@gmail.com>
2023-04-03 10:40:38 -0300

    fix: keymap overlapping

--- nvim/.config/nvim/after/plugin/vim-test.lua
+++ nvim/.config/nvim/after/plugin/vim-test.lua
@@ -1,3 +1,3 @@
 vim.keymap.set("n", "<leader>t", "<cmd>TestNearest<CR>")
 vim.keymap.set("n", "<leader>T", "<cmd>TestFile<CR>")
-vim.keymap.set("n", "<leader>a", "<cmd>TestSuite<CR>")
+vim.keymap.set("n", "<leader>A", "<cmd>TestSuite<CR>")
