84281690dbebe83f2dadcec7bb8f73a36845eefa
Luis Witz <luiswitz@gmail.com>
2023-04-03 09:21:19 -0300

    Include more treesitter parsers

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,12 +1,28 @@
 require('nvim-treesitter.configs').setup({
-	ensure_installed = { "rust", "lua", "javascript", "ruby", "vim", "typescript", "html", "css", "json", "scss", "sql", "yaml" },
-
-	sync_install = false,
-
-	auto_install = true,
-
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
+  ensure_installed = {
+    "css",
+    "diff",
+    "git_rebase",
+    "gitattributes",
+    "gitcommit",
+    "gitignore",
+    "glimmer",
+    "html",
+    "javascript",
+    "json",
+    "lua",
+    "ruby",
+    "rust",
+    "scss",
+    "sql",
+    "typescript",
+    "vim",
+    "yaml",
+  },
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
 })
