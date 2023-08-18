01bc94fc0afb5565acd815d8c259147a8f1cfea0
Luis Witz <luiswitz@gmail.com>
2023-08-18 15:57:58 -0300

    Use last build when running ember tests

--- nvim/.config/nvim/after/plugin/vim-test.lua
+++ nvim/.config/nvim/after/plugin/vim-test.lua
@@ -1,3 +1,5 @@
 vim.keymap.set("n", "<leader>t", "<cmd>TestNearest<CR>")
 vim.keymap.set("n", "<leader>T", "<cmd>TestFile<CR>")
 vim.keymap.set("n", "<leader>A", "<cmd>TestSuite<CR>")
+
+vim.g['test#javascript#ember#options'] = '--path dist'
