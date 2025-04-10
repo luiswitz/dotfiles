866b9406bdffdea7fb3e146fb22b4a926ee3cbc7
Luis Witz <luis@visible.vc>
2025-04-10 09:48:05 -0300

    Fix colors

--- nvim/.config/nvim/after/plugin/colorize.lua
+++ nvim/.config/nvim/after/plugin/colorize.lua
@@ -8,9 +8,6 @@ vim.cmd([[
 function Colorize(color)
 	color = color or "base16-material-darker"
 	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-Colorize('base16-material-darker')
+Colorize()
