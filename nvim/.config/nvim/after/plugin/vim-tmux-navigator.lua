ce253212c739b8d8381a2a6c9760670f7874136e
Luis Henrique Witz <luiswitz@gmail.com>
2025-12-19 16:31:28 -0300

    Fix Tmux Issues (#17)
    * Fix themes issues on Mac
    
    * Fix tmux navigation

--- nvim/.config/nvim/after/plugin/vim-tmux-navigator.lua
+++ nvim/.config/nvim/after/plugin/vim-tmux-navigator.lua
@@ -1,1 +1,11 @@
 vim.g.tmux_navigator_disable_when_zoomed = 1
+
+vim.keymap.del('n', '<C-h>', { silent = true })
+vim.keymap.del('n', '<C-j>', { silent = true })
+vim.keymap.del('n', '<C-k>', { silent = true })
+vim.keymap.del('n', '<C-l>', { silent = true })
+
+vim.keymap.set('n', '<C-h>', '<cmd>TmuxNavigateLeft<cr>', { silent = true })
+vim.keymap.set('n', '<C-j>', '<cmd>TmuxNavigateDown<cr>', { silent = true })
+vim.keymap.set('n', '<C-w>', '<cmd>TmuxNavigateUp<cr>', { silent = true })
+vim.keymap.set('n', '<C-l>', '<cmd>TmuxNavigateRight<cr>', { silent = true })
