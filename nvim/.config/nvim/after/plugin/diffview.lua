1a3505beb31cc7bbf231de6a26dbe1f431208d00
Luis Henrique Witz <luiswitz@gmail.com>
2026-02-21 10:38:08 -0300

    Add ReviewBranch and ReviewCommitHistory commands for branch review (#23)

--- nvim/.config/nvim/after/plugin/diffview.lua
+++ nvim/.config/nvim/after/plugin/diffview.lua
@@ -4,3 +4,11 @@ vim.keymap.set("n", "<leader>dv", ":DiffviewOpen<CR>", { desc = "Diffview: Open"
 vim.keymap.set("n", "<leader>dc", ":DiffviewClose<CR>", { desc = "Diffview: Close" })
 vim.keymap.set("n", "<leader>dh", ":DiffviewFileHistory %<CR>", { desc = "Diffview: File history (current file)" })
 vim.keymap.set("n", "<leader>dH", ":DiffviewFileHistory<CR>", { desc = "Diffview: File history (all)" })
+
+vim.api.nvim_create_user_command("ReviewBranch", function()
+  vim.cmd("DiffviewOpen origin/HEAD...HEAD --imply-local")
+end, { desc = "Review current branch changes against origin" })
+
+vim.api.nvim_create_user_command("ReviewCommitHistory", function()
+  vim.cmd("DiffviewFileHistory --range=origin/HEAD...HEAD --right-only --no-merges")
+end, { desc = "Review commit history of current branch against origin" })
