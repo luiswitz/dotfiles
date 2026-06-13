8ac7522f4e58833798e8c9c1c028ca23387287f2
Luis Henrique Witz <luiswitz@gmail.com>
2026-06-13 10:49:55 -0300

    Remove not used configs (#24)

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,28 +1,5 @@
-require('nvim-treesitter.configs').setup({
-  ensure_installed = {
-    "css",
-    "diff",
-    "git_rebase",
-    "gitattributes",
-    "gitcommit",
-    "gitignore",
-    "glimmer",
-    "html",
-    "javascript",
-    "json",
-    "lua",
-    "ruby",
-    "rust",
-    "scss",
-    "sql",
-    "typescript",
-    "vim",
-    "yaml",
-  },
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function(args)
+    pcall(vim.treesitter.start, args.buf)
+  end,
 })
